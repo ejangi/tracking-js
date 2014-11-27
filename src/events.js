01009c39e7b136543e0c9fa9cd4d54d9349bb8a5
James Angus <jamesa@suqld.org.au>
2014-11-27 14:17:13 +1000

    Added Event Tracking. More Careful Debug and Trace option checking.

---
+++ src/events.js
@@ -0,0 +1,86 @@
+TrackingJS.Events = ( function( parent ) {
+
+	var filetypes = /\.(zip|exe|dmg|pdf|doc.*|xls.*|ppt.*|mp3|txt|rar|wma|mov|avi|wmv|flv|wav)$/i,
+		anchors = parent.document.getElementsByTagName( "a" );
+
+	var clickHandler = function( event ) {
+		var el = event.target,
+			track = true,
+			href = ( typeof( el.href ) !== "undefined" ) ? el.href : "",
+			isThisDomain = href.match( document.domain.split( "." ).reverse()[1] + "." + document.domain.split( "." ).reverse()[0] );
+		
+		if ( !href.match( /^javascript:/i ) ) {
+			var elEv = []; elEv.value = 0, elEv.non_i = false;
+			if ( href.match( /^mailto\:/i ) ) {
+				elEv.category = "email";
+				elEv.action = "click";
+				elEv.label = href;
+				elEv.loc = href;
+			}
+			else if ( href.match( filetypes ) ) {
+				var extension = ( /[.]/.exec( href ) ) ? /[^.]+$/.exec(href) : undefined;
+				elEv.category = "download";
+				elEv.action = "click-" + extension[0];
+				elEv.label = href;
+				elEv.loc = href;
+			}
+			else if ( href.match( /^https?\:/i ) && !isThisDomain ) {
+				elEv.category = "external";
+				elEv.action = "click";
+				elEv.label = href;
+				elEv.loc = href;
+			}
+			else if ( href.match( /^#/i ) ) {
+				elEv.category = "bookmark";
+				elEv.action = "click";
+				elEv.label = href;
+				elEv.non_i = true;
+				elEv.loc = href;
+			}
+			else if ( href.match( /^tel\:/i ) ) {
+				elEv.category = "telephone";
+				elEv.action = "click";
+				elEv.label = href;
+				elEv.loc = href;
+			}
+			else {
+				track = false;
+			}
+
+			if (track) {
+				if ( elEv.non_i ) {
+					ga( "send", "event", elEv.category.toLowerCase(), elEv.action.toLowerCase(), elEv.label.toLowerCase(), elEv.value, { "nonInteraction": 1 } );
+				} else {
+					ga( "send", "event", elEv.category.toLowerCase(), elEv.action.toLowerCase(), elEv.label.toLowerCase(), elEv.value );
+				}
+
+				if ( el.target === undefined || el.target.toLowerCase() !== "_blank" ) {
+			    	setTimeout( function() { location.href = elEv.loc; }, 400 );
+			    	return false;
+				}
+			}
+			return true;
+		}
+	};
+
+	var trackAnchorClicks = function() {
+		for ( var a = 0; a < anchors.length; a++ ) {
+			anchors[a].addEventListener( "click", clickHandler, false );
+		}
+	};
+
+	var init = ( function() {
+		if ( parent.requires.indexOf( "events" ) === -1 ) {
+			return;
+		}
+
+		trackAnchorClicks();
+	})();
+
+	return {
+		filetypes: filetypes,
+		anchors: anchors,
+		clickHandler: clickHandler
+	};
+
+} ) ( TrackingJS || {} );
--- src/tracking.js
+++ src/tracking.js
@@ -8,36 +8,36 @@
  * Released under the MIT license
  * http://jquery.org/license
  */
-( function( window, document, undefined ) {
+var TrackingJS = ( function( window, document, undefined ) {
 	var metas = document.getElementsByTagName( "meta" ) || [];
 
-	function meta( key ) {
+	var meta = function( key ) {
 		for ( var i = 0; i < metas.length; i++ ) { 
 			if ( metas[i].getAttribute( "name" ) === key ) { 
 				return metas[i].getAttribute( "content" );
 			}
 		}
 		return "";
-	}
+	};
 
-	function metaslike( key ) {
+	var metasLike = function( key ) {
 		var metalre = new RegExp( "^" + key + ".*" ); 
-		var returns = []; 
+		var returns = [];
 		for ( var i = 0; i < metas.length; i++ ) { 
 			if ( metas[i].getAttribute( "name" ).match( metalre ) ) { 
 				returns.push( metas[i] );
 			}
 		} 
 		return returns;
-	}
+	};
 
-	function gaProtocol() {
+	var gaProtocol = function() {
 		var proto = "https:";
 		if ( window.location.protocol === "http:") {
 			proto = "http:";
 		}
 		return proto;
-	}
+	};
 
 	var analyticsUrl = "//www.google-analytics.com/analytics.js",
 		analyticsDebugUrl = "//www.google-analytics.com/analytics_debug.js",
@@ -47,57 +47,90 @@
 		debug = meta( "ga-debug" ) || false,
 		trace = meta( "ga-trace" ) || false,
 		campaignFields = [ "campaignName", "campaignSource", "campaignMedium", "campaignContent", "campaignKeyword" ],
-		dimensions = metaslike( "ga-dimension" ),
+		dimensions = metasLike( "ga-dimension" ),
 		userId = meta( "ga-userid" );
 
-	if ( trackingId !== null && trackingId.length > 0 ) {
-		if ( debug && debug === "true" ) {
-			analyticsUrl = analyticsDebugUrl;
-		}
-
-		if ( trace && trace === "true" ) {
-			window.ga_debug = { trace: true };
-		}
-
-		analyticsUrl = gaProtocol() + analyticsUrl;
-
-		(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-		})(window,document,"script",analyticsUrl,"ga");
-
-		if ( typeof(ga) !== "function" ) {
-			console.log("Tracking-JS failed. Google Analytics Object not loaded.");
-			return;
-		}
-
-		ga( "create", trackingId, "auto" );
-		
-		if ( requires.indexOf( "displayfeatures" ) > -1 ) {
-			ga( "require", "displayfeatures" );
-		}
-
-		if ( requires.indexOf( "linkid" ) > -1 ) {
-			linkidUrl = gaProtocol() + linkidUrl;
-			ga( "require", "linkid",  linkidUrl );
-		}
-
-		for ( var i = 0; i < campaignFields.length; i++ ) {
-			if ( meta( campaignFields[i] ) ) {
-				ga( "set", campaignFields[i], meta( campaignFields[i] ) );
+	var init = (function() {
+		if ( trackingId !== null && trackingId.length > 0 ) {
+			if ( debug && debug === "true" ) {
+				debug = true;
+			} else {
+				debug = false;
 			}
-		}
 
-		for ( var j = 0; j < dimensions.length; j++ ) {
-			if ( meta( dimensions[i] ) ) {
-				ga( "set", dimensions[i], meta( dimensions[i] ) );
+			if ( trace && trace === "true" ) {
+				trace = true;
+			} else {
+				trace = false;
 			}
-		}
 
-		if ( userId.length > 0 ) {
-			ga( "set", "&uid", userId );
+			if ( debug ) {
+				analyticsUrl = analyticsDebugUrl;
+			}
+
+			if ( trace ) {
+				window.ga_debug = { trace: true };
+			}
+
+			analyticsUrl = gaProtocol() + analyticsUrl;
+
+			(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+			})(window,document,"script",analyticsUrl,"ga");
+
+			if ( typeof(ga) !== "function" ) {
+				console.log("Tracking-JS failed. Google Analytics Object not loaded.");
+				return;
+			}
+
+			ga( "create", trackingId, "auto" );
+			
+			if ( requires.indexOf( "displayfeatures" ) > -1 ) {
+				ga( "require", "displayfeatures" );
+			}
+
+			if ( requires.indexOf( "linkid" ) > -1 ) {
+				linkidUrl = gaProtocol() + linkidUrl;
+				ga( "require", "linkid",  linkidUrl );
+			}
+
+			for ( var i = 0; i < campaignFields.length; i++ ) {
+				if ( meta( campaignFields[i] ) ) {
+					ga( "set", campaignFields[i], meta( campaignFields[i] ) );
+				}
+			}
+
+			for ( var j = 0; j < dimensions.length; j++ ) {
+				if ( meta( dimensions[i] ) ) {
+					ga( "set", dimensions[i], meta( dimensions[i] ) );
+				}
+			}
+
+			if ( userId.length > 0 ) {
+				ga( "set", "&uid", userId );
+			}
+			
+			ga( "send", "pageview" );
 		}
-		
-		ga( "send", "pageview" );
-	}
+	})();
+
+	return {
+		metas: metas,
+		meta: meta,
+		metasLike: metasLike,
+		gaProtocol: gaProtocol,
+		analyticsUrl: analyticsUrl,
+		analyticsDebugUrl: analyticsDebugUrl,
+		linkidUrl: linkidUrl,
+		trackingId: trackingId,
+		requires: requires,
+		debug: debug,
+		trace: trace,
+		campaignFields: campaignFields,
+		dimensions: dimensions,
+		userId: userId,
+		window: window,
+		document: document
+	};
 } ) ( window, document );
